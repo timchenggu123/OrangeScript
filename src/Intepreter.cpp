8d60e61b2dc6888ae2594aa182c1ed432817330a
18385963 <2013tim.g@gmail.com>
2019-08-27 21:58:03 -0400

    Intepreter WIP

--- src/Intepreter.cpp
+++ src/Intepreter.cpp
@@ -83,7 +83,7 @@ void Intepreter::intepret_node(Ast::Exp * node)
 				std::cerr << "Compiler: invalid value for the operation at ln:" << node->ln << " col:" << node->col << std::endl;
 				exit(1);
 			}
-			
+			break;
 		case OpType::SUB:
 			if (node->left->is_double_return ||
 				node->right->is_double_return) {
@@ -104,6 +104,7 @@ void Intepreter::intepret_node(Ast::Exp * node)
 				std::cerr << "Compiler: invalid value for the operation at ln:" << node->ln << " col:" << node->col << std::endl;
 				exit(1);
 			}
+			break;
 		case OpType::MUL:
 			if (node->left->is_double_return ||
 				node->right->is_double_return) {
@@ -124,6 +125,7 @@ void Intepreter::intepret_node(Ast::Exp * node)
 				std::cerr << "Compiler: invalid value for the operation at ln:" << node->ln << " col:" << node->col << std::endl;
 				exit(1);
 			}
+			break;
 		case OpType::DIV:
 			if (node->left->is_double_return ||
 				node->right->is_double_return) {
@@ -147,21 +149,249 @@ void Intepreter::intepret_node(Ast::Exp * node)
 				std::cerr << "Compiler: Invalid value for the operation at ln:" << node->ln << " col:" << node->col << std::endl;
 				exit(1);
 			}
+			break;
 		case OpType::MOD:
+			if (node->left->is_int_return && node->right->is_int_return) {
+				node->is_int_return = true;
+				int val1 = node->left->int_return;
+				int val2 = node->right->int_return;
+				node->int_return = val1 % val2;
+			}
+			else {
+				std::cerr <<"compiler: Modulus operator requires integer inputs at ln:" << node->ln << " col:" << node->col << std::endl;
+				exit(1);
+			}
+			break;
 		case OpType::EQ:
+			if ((node->left->is_int_return && node->right->is_int_return)) {
+				node->is_int_return = true;
+				int val1 = node->left->int_return;
+				int val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 1;
+				}
+				else {
+					node->int_return = 0;
+				}
+				
+			} else if ((node->left->is_double_return && node->right->is_int_return)) {
+				node->is_int_return = true;
+				double val1 = node->left->int_return;
+				int val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 1;
+				}
+				else {
+					node->int_return = 0;
+				}
+			}
+			else if ((node->left->is_double_return && node->right->is_double_return)) {
+				node->is_int_return = true;
+				double val1 = node->left->int_return;
+				double val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 1;
+				}
+				else {
+					node->int_return = 0;
+				}
+			}
+			else if ((node->left->is_int_return && node->right->is_double_return)) {
+				node->is_int_return = true;
+				int val1 = node->left->int_return;
+				double val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 1;
+				}
+				else {
+					node->int_return = 0;
+				}
+			}
+			else {
+				std::cerr << "Compiler: Invalid value for the operation at ln:" << node->ln << " col:" << node->col << std::endl;
+				exit(1);
+			}
+			break;
 		case OpType::NOT_EQ:
+			if ((node->left->is_int_return && node->right->is_int_return)) {
+				node->is_int_return = true;
+				int val1 = node->left->int_return;
+				int val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 0;
+				}
+				else {
+					node->int_return = 1;
+				}
+
+			}
+			else if ((node->left->is_double_return && node->right->is_int_return)) {
+				node->is_int_return = true;
+				double val1 = node->left->int_return;
+				int val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 0;
+				}
+				else {
+					node->int_return = 1;
+				}
+			}
+			else if ((node->left->is_double_return && node->right->is_double_return)) {
+				node->is_int_return = true;
+				double val1 = node->left->int_return;
+				double val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 0;
+				}
+				else {
+					node->int_return = 1;
+				}
+			}
+			else if ((node->left->is_int_return && node->right->is_double_return)) {
+				node->is_int_return = true;
+				int val1 = node->left->int_return;
+				double val2 = node->right->int_return;
+				if (val1 == val2) {
+					node->int_return = 0;
+				}
+				else {
+					node->int_return = 1;
+				}
+			}
+			else {
+				std::cerr << "Compiler: Invalid value for the operation at ln:" << node->ln << " col:" << node->col << std::endl;
+				exit(1);
+			}
+			break;
+
 		case OpType::L_AND:
+			//we first convert left and right to boolean (int 1 or 0) values
+			node->left->is_int_return == true;
+			node->right->is_int_return == true;
+			if (node->left->is_str_return) {
+				if (node->left->str_return == "") {
+					node->left->int_return = 0;
+				}
+				else {
+					node->left->int_return = 1;
+				}
+			}
+			else if (node->left->is_double_return) {
+				if (node->left->double_return == 0) {
+					node->left->int_return = 0;
+				}
+				else {
+					node->left->int_return = 1;
+				}
+			}
+			else {
+				std::cerr << "Compiler: Invalid input values for operation at ln:" << node->ln << " col:" << node->col;
+				exit(1);
+			}
+
+			if (node->right->is_str_return) {
+				if (node->right->str_return == "") {
+					node->right->int_return = 0;
+				}
+				else {
+					node->right->int_return = 1;
+				}
+			}
+			else if (node->right->is_double_return) {
+				if (node->right->double_return == 0) {
+					node->right->int_return = 0;
+				}
+				else {
+					node->right->int_return = 1;
+				}
+			}
+			else {
+				std::cerr << "Compiler: Invalid input values for operation at ln:" << node->ln << " col:" << node->col;
+				exit(1);
+			}
+
+
+			//Now we are ready to do the comparison.
+			if (node->left->int_return && node->right->int_return) {
+				node->int_return = 1;
+			}
+			else {
+				node->int_return = 0;
+			}
+			break;
 		case OpType::L_OR:
+			//we first convert left and right to boolean (int 1 or 0) values
+			node->left->is_int_return == true;
+			node->right->is_int_return == true;
+			if (node->left->is_str_return) {
+				if (node->left->str_return == "") {
+					node->left->int_return = 0;
+				}
+				else {
+					node->left->int_return = 1;
+				}
+			}
+			else if (node->left->is_double_return) {
+				if (node->left->double_return == 0) {
+					node->left->int_return = 0;
+				}
+				else {
+					node->left->int_return = 1;
+				}
+			}
+			else {
+				std::cerr << "Compiler: Invalid input values for operation at ln:" << node->ln << " col:" << node->col;
+				exit(1);
+			}
+
+			if (node->right->is_str_return) {
+				if (node->right->str_return == "") {
+					node->right->int_return = 0;
+				}
+				else {
+					node->right->int_return = 1;
+				}
+			}
+			else if (node->right->is_double_return) {
+				if (node->right->double_return == 0) {
+					node->right->int_return = 0;
+				}
+				else {
+					node->right->int_return = 1;
+				}
+			}
+			else {
+				std::cerr << "Compiler: Invalid input values for operation at ln:" << node->ln << " col:" << node->col;
+				exit(1);
+			}
+
+
+			//Now we are ready to do the comparison.
+			if (node->left->int_return || node->right->int_return) {
+				node->int_return = 1;
+			}
+			else {
+				node->int_return = 0;
+			}
+			break;
 		case OpType::B_OR:
+			break;
 		case OpType::B_AND:
+			break;
 		case OpType::LT:
+			break;
 		case OpType::GT:
+			break;
 		case OpType::LTE:
+			break;
 		case OpType::GTE:
+			break;
 		case OpType::ASSIGN:
+			break;
 
 		}
 
+		//End Operation, clear the return vals.
 		Ast::clearReturnVal(node->left);
 		Ast::clearReturnVal(node->right);
 		break;
@@ -226,9 +456,14 @@ int Intepreter::code_block_walk(Ast::Exp * node)
 				//TODO grab the variable table and check variable value
 			}
 
-			if (iterate_args(node) == 1){
+			//TODO: might need to expnd this
+			if (iterate_args(node) == BREAK){
 				break;
 			}
+			else if (iterate_args(node) == CONTINUE) {
+				continue;
+			}
+
 		}
 
 
